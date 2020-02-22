a38c0c7a756ff107a3fb8335de78ec7c3d7f46c9
Kate Fulton <katefulton@richard.lan>
2020-02-21 21:59:25 -0800

    Additional helper methods, clean up unneeded parentheses

--- src/ToBinary.elm
+++ src/ToBinary.elm
@@ -1,44 +1,59 @@
 module ToBinary exposing (..)
 
 import Html exposing (Html)
-import Html.Events exposing (..)
-import Html.Attributes exposing (..)
+import Html.Events
+import Html.Attributes
 import List
 import List.FlatMap exposing (flatMap)
 import Browser
 import Browser
 import String
 import Char
-import Binary exposing (toIntegers, fromDecimal)
+import Binary
 
 main = Browser.sandbox { init = init, update = update, view = view }
 
-type alias Binary = (String, List Int)
+type alias Model = (String, List (List Int))
 
-init : Binary
+init : Model
 init = ("", [])
 
 type Msg = ToAscii | ToBinary | CaptureInput String
 
+toWords: String -> List String
+toWords = String.split " "
+
+translator: List String -> (String -> List Int) -> List (List Int)
+translator words codeConverter = List.map codeConverter words
+
+concatList: List Int -> String
+concatList list = String.concat (List.map (String.fromInt) list)
+
 toAsciiHelper: String  -> List Int
-toAsciiHelper w = List.map (\c -> Char.toCode c) (String.toList w)
+toAsciiHelper word = List.map Char.toCode (String.toList word)
 
 toBinaryHelper: String -> List Int
-toBinaryHelper w = flatMap (\i -> toIntegers(fromDecimal i)) (toAsciiHelper w)
+toBinaryHelper word = flatMap (\i -> Binary.toIntegers (Binary.fromDecimal i)) (toAsciiHelper word)
 
-update : Msg -> Binary -> Binary
+update : Msg -> Model -> Model
 update msg (inp, _)  =
     case msg of
-        ToAscii -> (inp, toAsciiHelper inp)
-        ToBinary -> (inp, toBinaryHelper inp)
+        ToAscii -> (inp, translator (toWords inp) toAsciiHelper)
+        ToBinary -> (inp, translator (toWords inp) toBinaryHelper)
         CaptureInput w -> (w, [])
 
-view : Binary -> Html Msg
+codeToHtmlMsg: List (List Int) -> List (Html Msg)
+codeToHtmlMsg lists = List.map (toHtmlMsgHelper) (List.map (concatList) lists)
+
+toHtmlMsgHelper: String -> Html Msg
+toHtmlMsgHelper string = Html.div [] [Html.text string]
+
+view : Model -> Html Msg
 view (word, translation) =
     Html.div []
        [
        Html.input [ Html.Events.onInput (\inp -> CaptureInput inp), Html.Attributes.value word] []
        , Html.button [ Html.Events.onClick ToAscii ] [ Html.text "to ASCII"]
        , Html.button [ Html.Events.onClick ToBinary ] [ Html.text "to Binary"]
-       , Html.ul [] (List.map (\item -> Html.li [] [Html.text (String.fromInt item)]) translation)
+       , Html.div [] (codeToHtmlMsg translation)
        ]
