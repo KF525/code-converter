96deed3b22e7b7faf2fd895f2f66db06a1ccd237
Kate Fulton <katefulton@Richard.domain>
2020-02-21 18:31:44 -0800

    WIP: Binary. Controlled input.

--- src/ToBinary.elm
+++ src/ToBinary.elm
@@ -1,13 +1,14 @@
 module ToBinary exposing (..)
 
-import List exposing (map)
+import Html exposing (Html)
+import Html.Events exposing (..)
+import Html.Attributes exposing (..)
+import List
 import List.FlatMap exposing (flatMap)
 import Browser
-import Html exposing (Html, button, div, input, li, text, ul)
 import Browser
-import String exposing (toList, split)
-import Char exposing (toCode)
-import Html.Events exposing (onClick, onInput)
+import String
+import Char
 import Binary exposing (toIntegers, fromDecimal)
 
 main = Browser.sandbox { init = init, update = update, view = view }
@@ -17,27 +18,27 @@ type alias Binary = (String, List Int)
 init : Binary
 init = ("", [])
 
-type Msg = ToAscii String | ToBinary String | CaptureInput String
+type Msg = ToAscii | ToBinary | CaptureInput String
 
 toAsciiHelper: String  -> List Int
-toAsciiHelper w = map (\c -> Char.toCode c) (String.toList w)
+toAsciiHelper w = List.map (\c -> Char.toCode c) (String.toList w)
 
 toBinaryHelper: String -> List Int
 toBinaryHelper w = flatMap (\i -> toIntegers(fromDecimal i)) (toAsciiHelper w)
 
 update : Msg -> Binary -> Binary
-update msg inp  =
+update msg (inp, _)  =
     case msg of
-        ToAscii w -> (w, toAsciiHelper w)
-        ToBinary w -> (w, toBinaryHelper w)
+        ToAscii -> (inp, toAsciiHelper inp)
+        ToBinary -> (inp, toBinaryHelper inp)
         CaptureInput w -> (w, [])
 
 view : Binary -> Html Msg
-view word =
-    div []
+view (word, translation) =
+    Html.div []
        [
-       input [ onInput (\inp -> CaptureInput inp) ] []
-       , button [ onClick (ToAscii (Tuple.first word))] [ text "to ASCII"]
-       , button [ onClick (ToBinary (Tuple.first word))] [ text "to Binary"]
-       , ul [] (map (\item -> li [] [text (String.fromInt item)]) (Tuple.second(word)))
+       Html.input [ Html.Events.onInput (\inp -> CaptureInput inp), Html.Attributes.value word] []
+       , Html.button [ Html.Events.onClick ToAscii ] [ Html.text "to ASCII"]
+       , Html.button [ Html.Events.onClick ToBinary ] [ Html.text "to Binary"]
+       , Html.ul [] (List.map (\item -> Html.li [] [Html.text (String.fromInt item)]) translation)
        ]
